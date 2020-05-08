89eb0b3d03d75f2e324a6762c5f28c444241aa41
Victor de la Cruz <vcg_cruz@hotmail.com>
2020-05-08 18:40:37 -0500

    initial project configuration

---
+++ src/main/java/com/conta/cloud/sat/SatCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.conta.cloud.sat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conta/cloud/sat/SatCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conta.cloud.sat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
