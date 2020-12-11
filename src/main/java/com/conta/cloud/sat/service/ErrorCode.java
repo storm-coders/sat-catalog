d19dbb3eec629923a58630783486f41c11bda476
Victor de la C <vcg_cruz@hotmail.com>
2020-12-11 06:54:27 -0600

    update exception values

--- src/main/java/com/conta/cloud/sat/service/ErrorCode.java
+++ src/main/java/com/conta/cloud/sat/service/ErrorCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 	INTERNAL(500, "Error al procesar petición"),
-	BAD_REQUEST(400, "Parametros no validos");
+	BAD_REQUEST(400, "Parametros no validos"),
+	NOT_FOUND(404, "No se encontro objeto");
+
 	private final Integer code;
 	private final String message;
 }
